c7fe05b33674c3d3f15be09da9ec719aafbe19ab
Pankhida <DDesai0751@gmail.com>
2023-01-03 22:43:20 +0000

    my homework week18

---
+++ src/main/java/com/petstore/constants/EndPoints.java
@@ -0,0 +1,35 @@
+package com.petstore.constants;
+
+/**
+ * Created by Jay
+ */
+public class EndPoints {
+
+    /**
+     * This is Endpoints of petstore api
+     */
+    public static final String GET_ALL_PET = "/pet";
+
+    public static final String GET_SINGLE_USER_BY_ID = "/{id}";
+
+    public static final String UPDATE_USER_BY_ID="/{id}";
+
+    public static final String DELETE_USER_BY_ID = "/{id}";
+
+    /**
+     * This is Endpoints of Authentication api
+     */
+    /**
+     * This is Endpoints of USer api
+     */
+    public static final String GET_ALL_USERS = "/user";
+
+    public static final String GET_SINGLE_USER_BY_NAME = "/{name}";
+
+    public static final String UPDATE_USER_BY_NAME="/{name}";
+
+    public static final String DELETE_USER_BY_NAME = "/{name}";
+
+
+
+}
